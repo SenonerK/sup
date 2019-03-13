c2a4acab586759f6ac08da4a533f4465d67746f9
Kevin <kevisen2@gmail.com>
2019-03-13 14:43:43 +0100

    AuthSrv JWT functions

---
+++ srv/auth/jwt/jwt.go
@@ -0,0 +1,75 @@
+package jwt
+
+import (
+	"errors"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/senonerk/sup/shared/aerr"
+)
+
+// UserClaims contains userid
+type UserClaims struct {
+	UserID string `json:"userid"`
+	jwt.StandardClaims
+}
+
+var secret string
+
+// GenerateToken creates jwt token with custom claims
+func GenerateToken(userID string, duration time.Duration) (string, error) {
+	if err := loadSecret(); err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, UserClaims{
+		userID,
+		jwt.StandardClaims{
+			Issuer:    "senonerk.sup.srv.auth",
+			ExpiresAt: time.Now().Add(duration).Unix(),
+		},
+	})
+
+	// Generate JWT Token
+	ts, err := token.SignedString([]byte(secret))
+	return ts, err
+}
+
+// ValidateToken verifies if token hasen't been changed and expired
+func ValidateToken(tokenString string) (string, error) {
+	if err := loadSecret(); err != nil {
+		return "", err
+	}
+
+	token, err := jwt.ParseWithClaims(tokenString, &UserClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(UserClaims)
+	if !ok || !token.Valid {
+		return "", &aerr.AppError{
+			Code:    400,
+			Message: "Invalid authentication token",
+			Source:  "senonerk.sup.srv.auth.jwt",
+		}
+	}
+
+	return claims.UserID, nil
+}
+
+func loadSecret() error {
+	if secret != "" {
+		return nil
+	}
+
+	if secret = os.Getenv("JWT_SECRET"); secret == "" {
+		return errors.New("JWT secret not set")
+	}
+
+	return nil
+}
