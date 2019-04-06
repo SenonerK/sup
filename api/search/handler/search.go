92e48085fdd824875accd0eced0aa742445e365d
Kevin <kevisen2@gmail.com>
2019-04-06 16:34:35 +0200

    Added SearchAPI

---
+++ api/search/handler/search.go
@@ -0,0 +1,54 @@
+package handler
+
+import (
+	"github.com/senonerk/sup/shared/http/util"
+	"github.com/senonerk/sup/srv/profile/proto/profile"
+
+	"github.com/senonerk/sup/shared/http/middlewares"
+
+	"github.com/gin-gonic/gin"
+	"github.com/micro/go-micro/client"
+)
+
+// searchAPI struct
+type searchAPI struct {
+	Client profile.ProfileService
+}
+
+// New retuens new handler for auth
+func New() *gin.Engine {
+	router := gin.Default()
+
+	router.Use(middlewares.ErrorReporter())
+	router.Use(middlewares.AuthenticatedRoute())
+
+	srv := searchAPI{
+		Client: profile.NewProfileService("senonerk.sup.srv.profile", client.DefaultClient),
+	}
+
+	a := router.Group("/search")
+	a.GET("/", srv.Search)
+
+	return router
+}
+
+func (api *searchAPI) Search(c *gin.Context) {
+	ctx := c.Request.Context()
+
+	query := c.Query("query")
+	if query == "" {
+		c.Error(util.ErrBadRequest())
+		return
+	}
+
+	p, err := api.Client.Search(ctx, &profile.SearchRequest{
+		Query: query,
+	})
+
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	util.Ok(c, p.Users)
+}
---
+++ api/search/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"time"
+
+	"github.com/micro/go-log"
+	"github.com/micro/go-web"
+
+	"github.com/senonerk/sup/api/search/handler"
+)
+
+func main() {
+	service := web.NewService(
+		web.Name("senonerk.sup.api.search"),
+		web.Version("0.0.1"),
+		web.Handler(handler.New()),
+		web.RegisterTTL(time.Second*40),
+		web.RegisterInterval(time.Second*20),
+	)
+
+	if err := service.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
